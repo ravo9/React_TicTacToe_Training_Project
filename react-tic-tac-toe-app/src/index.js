c52e0e60e90c7f0737d01bb75ae89537376c0af0
Rafal Ozog <rafalozog@gmail.com>
2019-11-12 00:00:53 +0000

    Tic-tac-toe: history of the game implemented.

--- react-tic-tac-toe-app/src/index.js
+++ react-tic-tac-toe-app/src/index.js
@@ -53,14 +53,26 @@ class Game extends React.Component {
         squares: Array(9).fill(null),
       }],
       xIsNext: true,
+      stepNumber: 0,
     };
   }
 
   render() {
 
-    const history = this.state.history;
-        const current = history[history.length - 1];
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+          const desc = move ?
+            'Go to move #' + move :
+            'Go to game start';
+            return (
+              <li key={move}>
+              <button onClick={() => this.jumpTo(move)}>{desc}</button>
+              </li>
+              );
+            });
 
         let status;
         if (winner) {
@@ -80,14 +92,21 @@ class Game extends React.Component {
             </div>
             <div className="game-info">
               <div>{status}</div>
-              <ol>{/* TODO */}</ol>
+                <ol>{moves}</ol>
             </div>
           </div>
         );
+  }
+
+      jumpTo(step) {
+        this.setState({
+          stepNumber: step,
+          xIsNext: (step % 2) === 0,
+        });
       }
 
       handleClick(i) {
-         const history = this.state.history;
+         const history = this.state.history.slice(0, this.state.stepNumber + 1);
          const current = history[history.length - 1];
          const squares = current.squares.slice();
          if (calculateWinner(squares) || squares[i]) {
@@ -98,10 +117,10 @@ class Game extends React.Component {
            history: history.concat([{
              squares: squares,
            }]),
+           stepNumber: history.length,
            xIsNext: !this.state.xIsNext,
          });
        }
-
 }
 
 function calculateWinner(squares) {
